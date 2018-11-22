e7ff7b95eb4c929d465e98496758c9b48516c3f5
Ole Ersoy <ole.ersoy@gmail.com>
2018-11-21 19:19:17 -0600

    Removed duplicate test

--- duplicates.js
+++ duplicates.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const index = fs.readFileSync('target/main/css/index.css');
-let selectors = index.toString().match(/\.-?[_a-zA-Z]+[\w-]*(?=[^{}]*\{)/g);
-
+let selectors = index.toString().match(/(?:^|[, ])\.-?[_a-zA-Z]+[\w-]*(?=[^{}]*\{)/gm);
 function hasDuplicates(array) {
     return (new Set(array)).size !== array.length;
 }
-//console.log(hasDuplicates(selectors));
+console.log(hasDuplicates(selectors));
 
 //console.dir(selectors);
 
 var sorted_arr = selectors.slice().sort();
- var results = [];
+var results = [];
 for (var i = 0; i < sorted_arr.length - 1; i++) {
     if (sorted_arr[i + 1] == sorted_arr[i]) {
         results.push(sorted_arr[i]);
